5e9f568353e32de09aff3c14eaa1e93a1afa69ab
Juma Antony <jumaantony840@gmail.com>
2023-07-21 12:05:08 +0300

    error handling

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { v4 as uuid } from 'uuid'
@@ -59,12 +59,20 @@ export class TasksService {
     }
 
     // fetching a single task by id
-    getTaskById(id: string): Task {     
-        return this.tasks.find(task => task.id === id)
+    getTaskById(id: string): Task {  
+        // try to get task otherwise throw error 404 not found
+        const found = this.tasks.find(task => task.id === id)
+
+        if (!found) {
+            throw new NotFoundException(`Task with ID "${id}" not found`)
+        }
+
+        return found
     }
 
     // delete a task
     deleteTask(id: string): void {
+        const found = this.getTaskById(id)
         this.tasks = this.tasks.filter(task => task.id !== id)
     }
 
