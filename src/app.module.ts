d92fa74e3849beff8b5da576e1912d7cb7f9dcf4
Juma Antony <jumaantony840@gmail.com>
2023-07-21 15:14:31 +0300

    configuring postgress db connection and creating first entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [TasksModule],
+  imports: [
+    TasksModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'nestuser',
+      password: 'nestuser',
+      database: 'nestdb',
+      autoLoadEntities: true, // this will load all entities in the entities folder
+      synchronize: true, // this will automatically create tables in the database
+    })],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule { }
