7e6c0c23a74d5d9bfc82c0e5b5cb7cd9a39dc884
Juma Antony <jumaantony840@gmail.com>
2023-07-24 15:36:27 +0300

    integrated JWT in the application

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -4,9 +4,12 @@ import { TasksService } from './tasks.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TasksRepository } from './tasks.repository';
 import { Task } from './task.entity';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Task])], // this will load all entities in the entities folder
+  imports: [
+    AuthModule,
+    TypeOrmModule.forFeature([Task])], // this will load all entities in the entities folder
   controllers: [TasksController],
   providers: [TasksService, TasksRepository],
 })
