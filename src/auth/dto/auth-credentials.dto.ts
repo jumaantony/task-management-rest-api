01c6de4660747b9339db7419b2255dd6ece45b4c
Juma Antony <jumaantony840@gmail.com>
2023-07-24 09:20:15 +0300

    Added credentials and password validation

--- src/auth/dto/auth-credentials.dto.ts
+++ src/auth/dto/auth-credentials.dto.ts
@@ -1,5 +1,18 @@
+import { MaxLength, IsString, MinLength, Matches } from "class-validator";
 
 export class AuthCredentialsDto {
+
+    @IsString()
+    @MinLength(4)
+    @MaxLength(20)
     username: string;
+
+    @IsString()
+    @MinLength(8)
+    @MaxLength(20)
+    @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+        message:'Your Password Is too weak'
+    })
     password: string;
 }
+
