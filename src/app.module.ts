754bd8d22dedd78d62629ebd7ccda3d559cd8933
Juma Antony <jumaantony840@gmail.com>
2023-07-24 09:09:24 +0300

    setting up auth module and environment variable

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,28 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
     TasksModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'db.sngygzwqmaqmnmcjvgys.supabase.co',
+      host: process.env.host,
       port: 5432,
       username: 'postgres',
-      password: 'Accessdenied@2023',
+      password: process.env.password,
       database: 'postgres',
       autoLoadEntities: true, // this will load all entities in the entities folder
       synchronize: true, // this will automatically create tables in the database
       
-    })],
+    }),
+    AuthModule],
   controllers: [],
   providers: [],
 })
